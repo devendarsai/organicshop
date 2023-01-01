d57a4de27990f2bd23dbf1c0be8eb058f6b3800a
Devendar Sai Thallam <programmingwithdev@gmail.com>
2023-01-01 05:05:39 -0500

    Added env of firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyANgOG16mq8kvlTOKgoRvgx-QZtz3hl_kg',
+    authDomain: 'organicshop-2aa75.firebaseapp.com',
+    projectId: 'organicshop-2aa75',
+    storageBucket: 'organicshop-2aa75.appspot.com',
+    messagingSenderId: '386198673211',
+    appId: '1:386198673211:web:2a762e0f86e2375655491e',
+    measurementId: 'G-HWBB80D2P4',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'organicshop-2aa75',
+    appId: '1:386198673211:web:2a762e0f86e2375655491e',
+    databaseURL: 'https://organicshop-2aa75-default-rtdb.firebaseio.com',
+    storageBucket: 'organicshop-2aa75.appspot.com',
+    apiKey: 'AIzaSyANgOG16mq8kvlTOKgoRvgx-QZtz3hl_kg',
+    authDomain: 'organicshop-2aa75.firebaseapp.com',
+    messagingSenderId: '386198673211',
+    measurementId: 'G-HWBB80D2P4',
+  },
+};
